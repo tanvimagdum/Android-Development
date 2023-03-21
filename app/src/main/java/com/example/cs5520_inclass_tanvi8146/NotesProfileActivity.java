086b0317cbd7d4b34f9d2c19c96a7cc51d2650ce
tanvimagdum <magdum.t@northeastern.edu>
2023-03-21 16:19:14 -0400

    Committing profile

--- app/src/main/java/com/example/cs5520_inclass_tanvi8146/NotesProfileActivity.java
+++ app/src/main/java/com/example/cs5520_inclass_tanvi8146/NotesProfileActivity.java
@@ -3,8 +3,13 @@ package com.example.cs5520_inclass_tanvi8146;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NotesProfileActivity extends AppCompatActivity {
+
+    private TextView txtNotesProfileName;
+    private TextView txtNotesProfileEmail;
+    private TextView txtNotesProfilePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,10 @@ public class NotesProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notes_profile);
         setTitle("Profile");
 
-
+        txtNotesProfileName = findViewById(R.id.txtNotesProfileName);
+        txtNotesProfileEmail = findViewById(R.id.txtNotesProfileEmail);
+        txtNotesProfilePassword = findViewById(R.id.txtNotesProfilePassword);
+        
 
 
     }
